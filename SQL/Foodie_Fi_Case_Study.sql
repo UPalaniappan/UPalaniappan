9863ec31ddedcfabc81b23bd6403304da5d667ea
Uma Palaniappan <115836818+UPalaniappan@users.noreply.github.com>
2023-07-02 21:48:50 -0500

    Update Foodie_Fi_Case_Study.sql

--- SQL/Foodie_Fi_Case_Study.sql
+++ SQL/Foodie_Fi_Case_Study.sql
@@ -27,7 +27,7 @@ Customer 8 - After the trial period the customer is in the basic plan and upgrad
 -- 1. How many customers has Foodie-Fi ever had?
 
 SELECT 
-		COUNT (DISTINCT customer_id) as total_customers
+	COUNT (DISTINCT customer_id) as total_customers
 FROM foodie_fi.subscriptions
 
 -- 2. What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value
